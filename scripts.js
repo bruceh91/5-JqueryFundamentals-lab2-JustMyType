387c726af976478e2447c0ed69c0aa3a14e71004
Bruce Harris <bruceharris91@gmail.com>
2018-01-13 17:54:25 -0600

    highlighting works for letters only

--- scripts.js
+++ scripts.js
@@ -1,38 +1,72 @@
-document.addEventListener("DOMContentLoaded", function(){
+document.addEventListener("DOMContentLoaded", function () {
 
 
-$('#keyboard-upper-container').hide();
+    $('#keyboard-upper-container').hide();
 
 
-$(window).keydown(function (e) {
-    if (e.keyCode === 16) {
-        $('#keyboard-upper-container').show();
-        $('#keyboard-lower-container').hide();
-    }
-  })
+    $(window).keydown(function (e) {
+        if (e.keyCode === 16) {
+            $('#keyboard-upper-container').show();
+            $('#keyboard-lower-container').hide();
+        }
+    })
 
-  $(window).keyup(function (e) {
-    if (e.keyCode === 16) {
-        $('#keyboard-upper-container').hide();
-        $('#keyboard-lower-container').show();
-    }
-  })
-
-// if (event.keyCode == 15) {
-//     $('#keyboard-upper-container').show();
-//     $('#keyboard-lower-container').hide();}
+    $(window).keyup(function (e) {
+        if (e.keyCode === 16) {
+            $('#keyboard-upper-container').hide();
+            $('#keyboard-lower-container').show();
+        }
+        
+    })
 
 
 
-// $('#keyboard-upper-container').toggle(function (event) {
-//     if (event.keycode == 15 ) {
-//         $('#keyboard-upper-container').show();
-//         $('#keyboard-lower-container').hide();
-//     }
-// });
+    $(window).keypress(function (e) {
+        let code = e.which;
+        $('#' + e.which).addClass('highlight');
+        $(window).keyup(function (e) {
+            let lowerCase = e.which + 32
+        $('#' + e.which).removeClass('highlight');
+        $('#' + lowerCase).removeClass('highlight');
+        
+    })
+           
+    })
+
+})
 
 
-});
+//     $(window).keyup(function (e) {
+//         let code = "'#" + e.keyCode + "'"
+//         $(code).removeClass('highlight');
+//         console.log(code);
+
+// })
+
+//     var code = e.keyCode || e.which;
+//  if(code == 13)
+
+//     $(window).keydown(function (e) {
+//         code = "'"+ e.which +"'"
+//             $(code).addClass('highlight');
+//     })
+
+//     $(window).keydown(function (e) {
+//         if (e.keyCode === 97) {
+//             $('#97').addClass('highlight');
+//         }
+//     })
+
+
+
+    // let release = $(window).keyup
+
+    // function highlight(press, release) {
+    //     let x = press + (function (e))
+    // }
+
+
+
 
 
 
