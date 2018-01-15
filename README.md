eb003c3516a887e93fe61048c55240d6977c74d8
Bruce Harris <bruceharris91@gmail.com>
2018-01-15 17:11:37 -0600

    complete

--- README.md
+++ README.md
@@ -13,10 +13,10 @@ Covalence
 		* First keyboard, lowercase keys, numbers and special characters accessed without shift
 		* Second, keys accessed while holding down the shift key
 		* Exclude keys like tab, delete, shift, command, ctl, caps, option/alt & enter
-* The lowercase keyboard should be the only one displayed when the page loads. Write code to hide the uppercase keyboard when the page loads.
+<!-- * The lowercase keyboard should be the only one displayed when the page loads. Write code to hide the uppercase keyboard when the page loads.
 * Write jQuery code to set up keyboard toggling:
   * While the shift key is held down, hide the lowercase keyboard and show the uppercase one
-  * When the shift key is released, show the lowercase keyboard and hide the uppercase one
+  * When the shift key is released, show the lowercase keyboard and hide the uppercase one 
 * When keys are pressed, they should be highlighted in the browser.
 		*Hint: the letters should be matched with the corresponding ascii code. The id value of the key in the html corresponds to the ASCII character code that you can access in the keyboard listener. For example, ascii value 65 is A, and 97 is a. Search for ASCII character chart to see a complete list of codes. Ask yourself: How do I determine which key was pressed on the keyboard when using a jQuery event listener?
 * The sentences in the provided array should be displayed at the top of the page one sentence at a time. Once the sentence has been completed, the next in line should appear. There is already a div with id="sentence" in your html file. This is where you will display each sentence one at a time.
@@ -34,7 +34,7 @@ let sentences = ['ten ate neite ate nee enet ite ate inet ent eate', 'Too ato to
     * At the end of each sentence, clear out the feedback div
 * The user's words per minute should be calculated and displayed on the screen when you reach the end of the last sentence.
     * Can be calculated by: `numberOfWords / minutes - 2 * numberOfMistakes`
-        * For number of words, you can just count how many words make up the sentences you were given and hard code that value.
+        * For number of words, you can just count how many words make up the sentences you were given and hard code that value.-->
 * There should be a delay so the user can see the score. Then ask the user whether they would like to play again.
     * If confirmed yes, reset the game back to the start.
     * If no, leave as is.
--- scripts.js
+++ scripts.js
@@ -10,7 +10,7 @@ document.addEventListener("DOMContentLoaded", function () {
             $('#keyboard-lower-container').hide();
         }
     })
-    // changes bacl to lowercase when shift is released
+    // changes back to lowercase when shift is released
     $(window).keyup(function (e) {
         if (e.keyCode === 16) {
             $('#keyboard-upper-container').hide();
@@ -22,116 +22,107 @@ document.addEventListener("DOMContentLoaded", function () {
 
     // highlights keys when pressed
     $(window).keypress(function (e) {
-        let code = e.which;
         $('#' + e.which).addClass('highlight');
-        //removes highlight                     //doesn't unhighlight non-letter characters
+        //removes highlight                   
         $(window).keyup(function (e) {
-            let lowerCase = e.which + 32
-            $('#' + e.which).removeClass('highlight');
-            $('#' + lowerCase).removeClass('highlight');
+            $('span').removeClass('highlight');
         })
 
     })
 
-
-
-
-
-
-    function click () {                    /////// resets game when button is clicked
-        $(window).click(function () {
-        sentenceCount = 0;
-        currentSentence = 0;
-        strokeCount = 1;
-        pxCount = 0;
-        nextDisplaySentence = 1;
-        timer = 0;
-        start = 0;
-        end = 0;
-        mistakes = 0;
-        console.log('wooohhhhoooo');
-        $('.sentence').replaceWith('<p class="sentence">' + sentences[0] + '</p>');
-        $('button#btn').replaceWith(sentences[0].charAt(0));
-        everything();
+    //resets game when button is clicked
+    function click () {                    
+        $('#btn').click(function () {
+        location.reload(true);   //refreshes
     })
 }
-
-
-
-    let sentences = ['ten ate neite ate nee enet ite ate inet ent eate', 'Too ato too nOt enot one totA not anot tOO aNot', 'oat itain oat tain nate eate tea anne inant nean', 'itant eate anot eat nato inate eat anot tain eat', 'nee ene ate ite tent tiet ent ine ene ete ene ate'];
-    //54 words
-
+    let sentences = ['ten ate neite ate nee enet ite ate inet ent eate', 'Too ato too nOt enot one totA not anot tOO aNot', 'oat itain oat tain nate eate tea anne inant nean', 'itant eate anot eat nato inate eat anot tain eat', 'nee ene ate ite tent tiet ent ine ene ete ene ate'];   
 
     //puts first sentence on screen
     $('#sentence').prepend('<p class="sentence">' + sentences[0] + '</p>');
+
     //puts first target letter on screen
     $('#target-letter').prepend(sentences[0].charAt(0));
 
     //increments when keystrokes are greater than the number of chars in the sentnece.
     let sentenceCount = 0;
+
     //shows current sentence number 
     let currentSentence = 0;
+
     //counts the number of key strokes by incrementing with every key press.
     let strokeCount = 1;
+
     //current position of yellow box
     let pxCount = 0;
+
     //value of next sentence to be displayed
     let nextDisplaySentence = 1;
 
-
-
-
+    //brings in xs and checks
     var right = 'checkMark.svg';
     var wrong = 'xMark.png'
 
-
-
+    //when this = 1 the timer starts
     let timer = 0;
 
-
+    // var for start time and end time
     let start = 0;
     let end = 0;
 
+    //counts number of mistakes
     let mistakes = 0;
 
+    
 
 function everything(){
     $(window).keypress(function (event) {
+
         //var for current sentence
         currentSentence = sentences[sentenceCount];
+
         //var for the length of current sentence
         let sentenceLength = currentSentence.length;
+
         //value of the next letter you need to type
         let letter = currentSentence.charAt(strokeCount);
 
+        //puts restart and stay button on screen
+        let restartBtn = '<button class="btn-danger" id="btn">Click here to play again</button>'
+        let stayBtn = '<button class="btn-success" id="btnTwo">Click here to not restart</button>'
 
 
-        if (sentenceCount >= sentences.length - 1 && strokeCount == currentSentence.length) {       ///////// section for displaying score
+        // section for displaying score
+        if (sentenceCount >= sentences.length - 1 && strokeCount == currentSentence.length) {       
             end = Date.now();
             finishTime = end - start;
             minutes = finishTime / 1000 / 60;
             totalTime = (54 / minutes - 2 * mistakes);
 
+            //takes away sentences and adds score
             $('.sentence').replaceWith('<p class="sentence">Your score was     ' + totalTime + '</p>');
-            $('#feedback').replaceWith('<button id="btn">Click here to play again</button>');
+
+            //displays buttons
+            $('#feedback').empty().css({'display': 'none'}).append(restartBtn, stayBtn).slideUp( 1000 ).delay( 2000 ).fadeIn( 1000 );
+            
+            //takes away target letter and yellow block when score is displayed
             $('#target-letter').replaceWith('');
             $('#yellow-block').replaceWith('');
-            click();
-
-
-
 
 
         } else if (strokeCount < sentenceLength) {
+
             //increases pxCount by the size of one letter
             pxCount += .72;
+            
             // moves the yellow box one letter to the right
             $('#yellow-block').css('margin-left', pxCount + 'em');
 
             //generates letters in middle of screen
             $('#target-letter').replaceWith('<div class="row col-lg-12 text-center" id="target-letter">' + letter + '</div>');
 
-            if (timer == 1) {
+            //starts timer
+            if (timer == 1) { 
                 start = Date.now();
             }
 
@@ -152,13 +143,9 @@ function everything(){
                     'height': '15px',
                     'width': '15px',
                 })
+                //counts mistakes
                 mistakes++;
             }
-
-
-
-
-
 
         } else if (strokeCount == sentenceLength) {
             //placeholder for next sentence to be displayed
@@ -183,11 +170,7 @@ function everything(){
         //increment the number of key strokes by incrementing with every key press.
         strokeCount++;
         timer++;
-
     })
-
-
-
 }
 
 everything();    /////// runs everything
