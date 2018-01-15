cb4d286d98ae1ac031815d3ede64cca2e1f03744
Bruce Harris <bruceharris91@gmail.com>
2018-01-14 22:56:12 -0600

    everything except reseting properly

--- scripts.js
+++ scripts.js
@@ -36,7 +36,31 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
 
+
+
+    function click () {                    /////// resets game when button is clicked
+        $(window).click(function () {
+        sentenceCount = 0;
+        currentSentence = 0;
+        strokeCount = 1;
+        pxCount = 0;
+        nextDisplaySentence = 1;
+        timer = 0;
+        start = 0;
+        end = 0;
+        mistakes = 0;
+        console.log('wooohhhhoooo');
+        $('.sentence').replaceWith('<p class="sentence">' + sentences[0] + '</p>');
+        $('button#btn').replaceWith(sentences[0].charAt(0));
+        everything();
+    })
+}
+
+
+
     let sentences = ['ten ate neite ate nee enet ite ate inet ent eate', 'Too ato too nOt enot one totA not anot tOO aNot', 'oat itain oat tain nate eate tea anne inant nean', 'itant eate anot eat nato inate eat anot tain eat', 'nee ene ate ite tent tiet ent ine ene ete ene ate'];
+    //54 words
+
 
     //puts first sentence on screen
     $('#sentence').prepend('<p class="sentence">' + sentences[0] + '</p>');
@@ -55,11 +79,23 @@ document.addEventListener("DOMContentLoaded", function () {
     let nextDisplaySentence = 1;
 
 
+
+
     var right = 'checkMark.svg';
     var wrong = 'xMark.png'
-    
 
 
+
+    let timer = 0;
+
+
+    let start = 0;
+    let end = 0;
+
+    let mistakes = 0;
+
+
+function everything(){
     $(window).keypress(function (event) {
         //var for current sentence
         currentSentence = sentences[sentenceCount];
@@ -68,7 +104,25 @@ document.addEventListener("DOMContentLoaded", function () {
         //value of the next letter you need to type
         let letter = currentSentence.charAt(strokeCount);
 
-        if (strokeCount < sentenceLength) {
+
+
+        if (sentenceCount >= sentences.length - 1 && strokeCount == currentSentence.length) {       ///////// section for displaying score
+            end = Date.now();
+            finishTime = end - start;
+            minutes = finishTime / 1000 / 60;
+            totalTime = (54 / minutes - 2 * mistakes);
+
+            $('.sentence').replaceWith('<p class="sentence">Your score was     ' + totalTime + '</p>');
+            $('#feedback').replaceWith('<button id="btn">Click here to play again</button>');
+            $('#target-letter').replaceWith('');
+            $('#yellow-block').replaceWith('');
+            click();
+
+
+
+
+
+        } else if (strokeCount < sentenceLength) {
             //increases pxCount by the size of one letter
             pxCount += .72;
             // moves the yellow box one letter to the right
@@ -76,6 +130,10 @@ document.addEventListener("DOMContentLoaded", function () {
 
             //generates letters in middle of screen
             $('#target-letter').replaceWith('<div class="row col-lg-12 text-center" id="target-letter">' + letter + '</div>');
+
+            if (timer == 1) {
+                start = Date.now();
+            }
 
             //checks if key pressed matches key needed
             if (currentSentence.charCodeAt(strokeCount - 1) == event.keyCode) {
@@ -94,10 +152,15 @@ document.addEventListener("DOMContentLoaded", function () {
                     'height': '15px',
                     'width': '15px',
                 })
+                mistakes++;
             }
-        } else if (currentSentence > sentences.length) {
-            return ""         ///function to return score will go here
-        } else if (strokeCount >= sentenceLength) {
+
+
+
+
+
+
+        } else if (strokeCount == sentenceLength) {
             //placeholder for next sentence to be displayed
             nextSentence = sentences[nextDisplaySentence];
             //replaces old sentence with new one
@@ -119,10 +182,15 @@ document.addEventListener("DOMContentLoaded", function () {
         }
         //increment the number of key strokes by incrementing with every key press.
         strokeCount++;
+        timer++;
 
     })
 
 
+
+}
+
+everything();    /////// runs everything
 
 
 
